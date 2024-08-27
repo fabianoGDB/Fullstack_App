b3e1ff0481fa2c6f1b66199d068c384dbd0158c1
fabianoGDB <doctorfabi.f@gmail.com>
2024-08-26 22:55:44 -0300

    Second commit

---
+++ Dima.Api/Program.cs
@@ -0,0 +1,52 @@
+using Dima.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(x => { x.UseSqlServer(connectionString); });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x => x.CustomSchemaIds(n => n.FullName));
+builder.Services.AddTransient<Handler>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapPost(
+    "/",
+    (Request request, Handler handler) => handler.Handle(request))
+.WithName("Transactions: Create")
+.WithSummary("Cria uma nova transação")
+.Produces<Response>();
+
+
+app.Run();
+
+
+public class Request
+{
+    public string Title { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public int Type { get; set; }
+    public decimal Amount { get; set; }
+    public long CtegoryId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+}
+
+public class Response
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+}
+
+public class Handler
+{
+    public Response Handle(Request request)
+    {
+        return new Response { Id = 4, Title = request.Title };
+    }
+}
---
+++ Dima.Core/Models/Category.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Models
+{
+    public class Category
+    {
+        public long Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+
+    }
+}
